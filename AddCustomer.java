8a9220cbae31a350e8fd3ebddde4beb0e7b8ac13
sanidhya suman <54212824+sanidhyasuman@users.noreply.github.com>
2023-05-12 21:25:02 +0530

    AddCustomer.java

--- AddCustomer.java
+++ AddCustomer.java
@@ -38,47 +38,20 @@ public class AddCustomer extends JFrame implements ActionListener {
         setLayout(null);
         
         JLabel heading=new JLabel("Add Customer Details");
-        heading.setBounds(320,30,500,50);
+        heading.setBounds(240,30,450,30);
         heading.setFont(new Font("SAN_SERIF",Font.BOLD,25));
         add(heading); 
         
         JLabel lbname=new JLabel("Name");
-        lbname.setBounds(50,150,150,30);
+        lbname.setBounds(80,150,150,30);
         lbname.setFont(new Font("serif",Font.PLAIN,20));
         add(lbname);
         
         tfname=new JTextField();
-        tfname.setBounds(200,150,150,30);
+        tfname.setBounds(200,150,150,20);
         add(tfname);
         
-        JLabel lbfname=new JLabel("Father Name");
-        lbfname.setBounds(400,150,200,30);
-        lbfname.setFont(new Font("serif",Font.PLAIN,20));
-        add(lbfname);
-        
-        tffname=new JTextField();
-        tffname.setBounds(550,150,150,30);
-        add(tffname);
-        
-        JLabel lbdob=new JLabel("Date Of Birth");
-        lbdob.setBounds(50,200,200,30);
-        lbdob.setFont(new Font("serif",Font.PLAIN,20));
-        add(lbdob);
-        
-        dcdob=new JDateChooser();
-        dcdob.setBounds(200,200,150,30);
-        add(dcdob);
-        
-        JLabel lbage=new JLabel("Age");
-        lbage.setBounds(400,200,200,30);
-        lbage.setFont(new Font("serif",Font.PLAIN,20));
-        add(lbage);
-        
-        String a[]={"18","19","20","21","22","23","24","25","26","27","28","29","30"};
-        age=new JComboBox(a);
-        age.setBackground(Color.WHITE);
-        age.setBounds(550,200,150,30);
-        add(age);
+
         
         JLabel lbmail=new JLabel("E-Mail");
         lbmail.setBounds(50,250,200,30);
@@ -90,12 +63,12 @@ public class AddCustomer extends JFrame implements ActionListener {
         add( tfmail);
         
         JLabel lbphone=new JLabel("Phone");
-        lbphone.setBounds(400,250,200,30);
+        lbphone.setBounds(50,200,200,30);
         lbphone.setFont(new Font("serif",Font.PLAIN,20));
         add(lbphone);
         
         tfphone=new JTextField();
-        tfphone.setBounds(550,250,150,30);
+        tfphone.setBounds(200,200,150,30);
         add(tfphone);
         
         JLabel lbaddress=new JLabel("Address");
@@ -108,17 +81,17 @@ public class AddCustomer extends JFrame implements ActionListener {
         add(tfaddress);
         
         JLabel cusId=new JLabel("Customer Id");
-        cusId.setBounds(400,300,200,30);
+        cusId.setBounds(50,350,200,30);
         cusId.setFont(new Font("serif",Font.PLAIN,20));
         add(cusId);
         
         custId=new JLabel("" + number);
-        custId.setBounds(550,300,200,30);
+        custId.setBounds(200,350,200,30);
         custId.setFont(new Font("serif",Font.PLAIN,20));
         add(custId);
         
         add=new JButton("Add Details");
-        add.setBounds(250,450,150,40 );
+        add.setBounds(100,450,150,40 );
 
         add.setBackground(Color.BLACK);
         add.setForeground(Color.WHITE);
@@ -126,14 +99,14 @@ public class AddCustomer extends JFrame implements ActionListener {
         add(add);
         
         back=new JButton("Back");
-        back.setBounds(450,450,150,40 );
+        back.setBounds(300,450,150,40 );
 
         back.setBackground(Color.BLACK);
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
         
-        setSize(900,700);
+        setSize(700,600);
         setLocation(300,50);
         setVisible(true);
     }
@@ -142,21 +115,19 @@ public class AddCustomer extends JFrame implements ActionListener {
       if(ae.getSource()==add)
         {
             String name = tfname.getText();
-            String fname = tffname.getText();
-            String dob=((JTextField) dcdob.getDateEditor().getUiComponent()).getText();
-            String Age= (String) age.getSelectedItem();
+
             String mail= tfmail.getText();
             String phone= tfphone.getText();
             String address= tfaddress.getText();
             String cusId= custId.getText();
-            if (name.equals("") || address.equals("") || fname.equals("")) {
+            if (name.equals("") || address.equals("") ) {
                 JOptionPane.showMessageDialog(null, "Please fill all details...");
             } else if (!isValidEmail(mail)) {
                 JOptionPane.showMessageDialog(null, "Invalid Email ");
             } else if (!isValidMobileNo(phone)) {
                 JOptionPane.showMessageDialog(null, "Invalid Phone....");
             } else {
-                String qry ="insert into customer values('"+name+"','"+fname+"','"+dob+"','"+Age+"',"
+                String qry ="insert into customer values('"+name+"',"
                         + "'"+mail+"','"+phone+"','"+address+"','"+cusId+"')";
                 try {
                     Conn c1 = new Conn();
